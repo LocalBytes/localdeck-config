aa39733f87ece9c1a43189e688031dc1c87d8151
Adam Allport <adam@allport.me.uk>
2024-07-15 15:50:08 +0100

    feat(codegen): Added ESLint

---
+++ packages/localdeck-codegen/eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+
+export default [
+    {files: ["**/*.{js,mjs,cjs,ts}"]},
+    {languageOptions: {globals: {...globals.browser, ...globals.node}}},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {plugins: {'@stylistic': stylistic}},
+];
