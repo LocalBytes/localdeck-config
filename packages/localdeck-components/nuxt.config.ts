c0a6420e9aed00a87eed4b23a1861675cef448bd
Adam Allport <adam@allport.me.uk>
2024-07-15 16:09:41 +0100

    feat(components): Added ESLint

--- packages/localdeck-components/nuxt.config.ts
+++ packages/localdeck-components/nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
     srcDir: "src",
-    modules: [
-        '@vueuse/nuxt',
-    ],
+    modules: ['@vueuse/nuxt', "@nuxt/eslint"],
+    compatibilityDate: "2024-07-15",
+
+    eslint: {config: {stylistic: true}},
+
+    // https://github.com/nuxt/nuxt/discussions/26739
+    nitro: {experimental: {legacyExternals: true}},
+
 })
