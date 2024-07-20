13e3e591816f8abde747446dcd07e4269c51b8b3
Adam Allport <adam@allport.me.uk>
2024-07-20 23:11:02 +0100

    fix(configurator): Zod

--- packages/localdeck-configurator/src/server/api/editor.get.ts
+++ packages/localdeck-configurator/src/server/api/editor.get.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs/promises';
 
 import { decompress } from '@localbytes/localdeck-components/src/utils/compression';
+import { zPadEditor } from '@localbytes/localdeck-components/src/utils/PadCfg';
 
 export default defineEventHandler(async (event) => {
   const { filesDir } = useRuntimeConfig();
