d56062db33155e7b8bd9b6d2f6feb69d9513df0e
Adam Allport <adam@allport.me.uk>
2024-08-02 17:54:14 +0100

    test(configurator): Add checks to ensure new config is empty

--- packages/localdeck-configurator/tests/e2e/file-import.test.ts
+++ packages/localdeck-configurator/tests/e2e/file-import.test.ts
@@ -46,7 +46,15 @@ describe('Import Workflow', async () => {
     // Load the page
     console.log('Loading Page');
     const page = (await createPage('/'));
+
+    const responsePromise = page.waitForResponse(r => r.url().includes('/api/editor'));
+
+    console.log('Clicking');
     await page.getByText(FILENAME).click();
+
+    const response = await responsePromise.then(r => r.json());
+    expect(response.configStr).toBeNull();
+    expect(response.config).toMatchObject({ title: 'LocalBytes LocalDeck 9751c4', buttons: {} });
 
     // Update the friendly name
     console.log('Updating Friendly Name');
