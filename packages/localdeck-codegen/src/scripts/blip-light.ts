3730f321778f4aacc4659dfefc23a85b36ec1c2a
Michael Cristina <mcristina422@users.noreply.github.com>
2024-08-25 11:34:36 -0500

    Use native repeat action for bliplights

--- packages/localdeck-codegen/src/scripts/blip-light.ts
+++ packages/localdeck-codegen/src/scripts/blip-light.ts
@@ -1,25 +1,33 @@
 import {Script} from "esphome-config-ts/dist/components/index.js";
 import {lambda} from "esphome-config-ts/dist/yaml/index.js";
 
-const fadeOut = Array.from({length: 20}, (_, i) => i)
-    .map(i => Math.max(100 - (i * 5), 0))
-    .flatMap(brightness => [{
-        'light.addressable_set': {
-            id: "ledstrip",
-            range_from: lambda('return led_index;'),
-            range_to: lambda('return led_index;'),
-            red: `${brightness}%`,
-            green: `${brightness}%`,
-            blue: `${brightness}%`,
-            white: `${brightness}%`
-        },
-    }, {
-        'delay': '25ms'
-    }])
+// How many steps to take when dimming the lights
+const iterations = '20';
+
+// Brightness calculation. Note "iteration" is a counter the repeat action provides.
+// https://esphome.io/automations/actions.html#repeat-action
+const brightness = lambda(`return 1.0 - ((float)iteration / ${iterations});`);
 
 export const scriptBlipLight = new Script({
     id: "blip_light",
-    parameters: {led_index: 'int'},
+    parameters: { led_index: 'int' },
     mode: "parallel",
-    then: fadeOut
+    then: [{
+        'repeat': {
+            'count': iterations,
+            'then': [{
+                'light.addressable_set': {
+                    id: "ledstrip",
+                    range_from: lambda('return led_index;'),
+                    range_to: lambda('return led_index;'),
+                    red: brightness,
+                    green: brightness,
+                    blue: brightness,
+                    white: brightness,
+                },
+            }, {
+                'delay': '25ms'
+            }]
+        }
+    }]
 });
