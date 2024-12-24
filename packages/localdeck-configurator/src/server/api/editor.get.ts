80f631407cb32651eea24c424c5d334e930533f1
Adam Allport <adam@allport.me.uk>
2024-12-24 11:47:57 +0000

    feat: Restored additional items

--- packages/localdeck-configurator/src/server/api/editor.get.ts
+++ packages/localdeck-configurator/src/server/api/editor.get.ts
@@ -8,20 +8,27 @@ export default defineEventHandler(async (event) => {
   const { filesDir } = useRuntimeConfig();
   const { filename } = getQuery(event);
   const content = fs.createReadStream(`${filesDir}/${filename}`, 'utf8');
-  const rl = readline.createInterface({ input: content });
+  const rl = readline.createInterface({ input: content, crlfDelay: Infinity });
 
-  let configStr: null | string = null;
+  let configMatch = null;
+  let matchName = null;
+  let matchFriendly = null;
 
   for await (const line of rl) {
-    const configMatch = line.match(/configurator\?config=(.*)/);
-    configStr = configMatch ? decodeURIComponent(configMatch[1]) : null;
-    if (configStr) break;
+    configMatch ??= line.match(/localdeck-configurator\?config=(.*)/);
+    matchName ??= line.match(/ name:? ?"(.*)?"/);
+    matchFriendly ??= line.match(/ friendly_name:? ?"(.*)?"/);
+    // noinspection PointlessBooleanExpressionJS
+    if (configMatch) break;
   }
+
+  const configStr = configMatch ? decodeURIComponent(configMatch[1]) : null;
 
   const config = configStr
     ? decompress(configStr, zPadEditor)
     : zPadEditor.parse({});
 
+  config.title = matchFriendly?.[1] ?? matchName?.[1] ?? config.title ?? 'My LocalDeck';
   config.buttons ??= {};
 
   return { configStr, config };
