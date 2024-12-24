cfecacac5ce7bf0afc6b2aca0708b2420a396d5b
Adam Allport <adam@allport.me.uk>
2024-12-24 11:15:04 +0000

    feat: Read in line by line to find data

--- packages/localdeck-configurator/src/server/api/editor.get.ts
+++ packages/localdeck-configurator/src/server/api/editor.get.ts
@@ -1,4 +1,5 @@
-import * as fs from 'fs/promises';
+import * as fs from 'node:fs';
+import * as readline from 'node:readline';
 
 import { decompress } from '@localbytes/localdeck-components/src/utils/compression';
 import { zPadEditor } from '@localbytes/localdeck-components/src/utils/PadCfg';
@@ -6,21 +7,22 @@ import { zPadEditor } from '@localbytes/localdeck-components/src/utils/PadCfg';
 export default defineEventHandler(async (event) => {
   const { filesDir } = useRuntimeConfig();
   const { filename } = getQuery(event);
-  const content = await fs.readFile(`${filesDir}/${filename}`, 'utf8');
-  const configMatch = content.match(/configurator\?config=(.*)/);
-  const configStr = configMatch ? decodeURIComponent(configMatch[1]) : null;
+  const content = fs.createReadStream(`${filesDir}/${filename}`, 'utf8');
+  const rl = readline.createInterface({ input: content });
+
+  let configStr: null | string = null;
+
+  for await (const line of rl) {
+    const configMatch = line.match(/configurator\?config=(.*)/);
+    configStr = configMatch ? decodeURIComponent(configMatch[1]) : null;
+    if (configStr) break;
+  }
 
   const config = configStr
     ? decompress(configStr, zPadEditor)
     : zPadEditor.parse({});
 
-  const matchName = content.match(/name: (.*)/);
-  if (matchName) config.title = matchName[1];
-
-  const matchFriendly = content.match(/friendly_name: (.*)/);
-  if (matchFriendly) config.title = matchFriendly[1];
-
   config.buttons ??= {};
 
-  return { configStr, config, content };
+  return { configStr, config };
 });
